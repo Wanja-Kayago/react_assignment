bc36fd5dab1b736b532fce03686ccc0c1cac86cc
Wanja-Kayago <160125141+Wanja-Kayago@users.noreply.github.com>
2024-06-21 06:31:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/About/index.js
@@ -0,0 +1,15 @@
+import './index.css';
+
+const About = () =>{
+    return(
+        <div>
+            <div className="about-text">
+            <p>We are learning about JSX</p>
+            <p>We are coding in a new Front-end library</p>
+        </div>
+        </div>
+    )
+}
+
+export default About;
+
